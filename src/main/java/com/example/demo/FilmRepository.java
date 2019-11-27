1e288c1c7e955a04e7c6a569664b6397a05beb82
eladiiviolina <eladiiviolina@gmail.com>
2019-11-27 17:24:26 +0200

    Am adaugat fisierele

---
+++ src/main/java/com/example/demo/FilmRepository.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+    public class FilmRepository {
+        List<String> films = new ArrayList<>();
+
+        public FilmRepository() {
+            films.add("Harry Potter ");
+            films.add("Hatiko");
+            films.add("Holywood");
+            films.add("Tomb Raider");
+            films.add("Healer");
+        }
+
+        public List<String> getFilms(){
+            return films;
+        }
+
+        public void addFilms(String name){
+            films.add(name);
+        }
+
+    }
